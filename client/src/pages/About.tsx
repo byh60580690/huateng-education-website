6199029882627134f48a64174256f22da8202041
BryanBai001 <u3602025@connect.hku.hk>
2026-06-14 23:38:30 +0800

    0614_v16

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -22,8 +22,8 @@ const teamPhotos = [
   '/images/team/team_6.jpg',
   '/images/team/team_3.jpg',
   '/images/team/team_4.jpg',
-  '/images/team/team_5.jpg',
   '/images/team/team_7.jpg',
+  '/images/team/team_5.jpg',
 ];
 
 export default function About() {
